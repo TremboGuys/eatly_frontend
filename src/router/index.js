aa17e927660a60737571e3921f642ffd9f0cc59b
Joaovictor23t4 <joaovictor239090@gmail.com>
2025-05-16 19:24:38 -0300

    FEAT: Create default routes and views #4

--- src/router/index.js
+++ src/router/index.js
@@ -7,8 +7,43 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView,
+      component: () => import('@/views/HomeView.vue')
     },
+    {
+      path: '/',
+      chieldren: [
+        {
+          path: 'dashboard',
+          name: 'dashboard',
+          component: () => import('@/views/DashboardView.vue')
+        },
+      ]
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: () => import('@/views/auth/SignUpView.vue')
+    },
+    {
+      path: '/signin',
+      name: 'signin',
+      component: () => import('@/views/auth/SignInView.vue')
+    },
+    {
+      path: '/restaurant/:id',
+      name: 'restaurant',
+      component: () => import('@/views/restaurant/RestaurantView.vue')
+    },
+    {
+      path: '/restaurant/:id/reviews',
+      name: 'reviews',
+      component: () => import('@/views/restaurant/ReviewsView.vue')
+    },
+    {
+      path: '/restaurant/:id/reviews/create',
+      name: 'createReview',
+      component: () => import('@/views/restaurant/CreateReviewView.vue')
+    }
   ],
 })
 
