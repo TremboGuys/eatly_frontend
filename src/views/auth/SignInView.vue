59c6f52e16e39f4717b0857c697f4dc2f6ab24e9
LuizBlume <luiz.fernando.251107@gmail.com>
2025-05-17 00:00:24 -0300

    FEAT: Create signIn page #9.

--- src/views/auth/SignInView.vue
+++ src/views/auth/SignInView.vue
@@ -1,9 +1,54 @@
 <script setup>
+import InputEmail from '@/components/signIn/InputEmail.vue';
+import PasswordInput from '@/components/signIn/PasswordInput.vue';
+import SendButton from '@/components/signIn/SendButton.vue';
+import FaceButton from '@/components/signIn/FaceButton.vue';
+import GoogleButton from '@/components/signIn/GoogleButton.vue';
 </script>
 
 <template>
-  <div></div>
+  <div class="container">
+    <img src="@/assets/img/logo.png" class="logo" alt="">
+    <h1 class="title">Bem vindo de volta!</h1>
+    <div class="hr"></div>
+    <InputEmail />
+    <PasswordInput />
+    <SendButton />
+    <div class="or">Ou</div>
+    <div class="hr"></div>
+    <FaceButton />
+    <GoogleButton />
+  </div>
 </template>
 
 <style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+.logo {
+  width: 180px;
+  height: auto;
+  margin-bottom: 2rem;
+}
+.title {
+  font-size: 2rem;
+  font-weight: bold;
+  margin-bottom: 1rem;
+  font-family: 'Poppins', sans-serif;
+}
+.hr {
+  width: 100%;
+  height: 1px;
+  background-color: #ccc;
+  margin-bottom: 2rem;
+}
+.or {
+  font-size: 1.2rem;
+  margin: 1rem 0;
+  font-family: 'Poppins', sans-serif;
+  color: #666;
+}
 </style>
