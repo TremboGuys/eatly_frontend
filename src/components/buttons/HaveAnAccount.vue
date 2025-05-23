2b8d4b51630f04c890970f4d8f329adc043b1a36
LuizBlume <luiz.fernando.251107@gmail.com>
2025-05-23 14:17:26 -0300

    FIX: Adjust in computed function in router #14.

--- src/components/buttons/HaveAnAccount.vue
+++ src/components/buttons/HaveAnAccount.vue
@@ -5,10 +5,9 @@ import { computed } from 'vue'
 const route = useRoute()
 
 const isOnSignIn = computed(() => route.name === 'signin')
-const isOnSignUp = computed(() => route.name === 'signup')
 </script>
 <template>
-    <h2 v-if="isOnSignUp">Não possui uma conta? <router-link class="link" to="/signin">Entrar</router-link></h2>
+    <h2 v-if="!isOnSignIn">Não possui uma conta? <router-link class="link" to="/signin">Entrar</router-link></h2>
     <h2 v-else-if="isOnSignIn">Já possui uma conta? <router-link class="link" to="/signup">Cadastrar</router-link></h2>
 </template>
 
