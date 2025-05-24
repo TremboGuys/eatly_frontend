10dfa3ee822192d99752b3087c95b9d9b9c052d2
dnzlucas <lucasdefenix@gmail.com>
2025-05-24 01:24:07 -0300

    FEAT: Create icons and impor in navigation bar (for default layout) #7

--- src/components/navigationBar/NavIcons.vue
+++ src/components/navigationBar/NavIcons.vue
@@ -1,38 +1,50 @@
 <script setup>
 
-import search from '@/assets/img/navigationBar/search.png';
-import person from '@/assets/img/navigationBar/user.png';
-import home from '@/assets/img/navigationBar/home.png';
-import cart from '@/assets/img/navigationBar/cart.png';
-import heart from '@/assets/img/navigationBar/heart.png';
+import { RouterLink } from 'vue-router';
+import search from '@/assets/img/navigationBar/search.svg?component';
+import profile from '@/assets/img/navigationBar/profile.svg?component';
+import home from '@/assets/img/navigationBar/home.svg?component';
+import cart from '@/assets/img/navigationBar/cart.svg?component';
+import heart from '@/assets/img/navigationBar/heart.svg?component';
 
-const icons = [
+const buttons = [
     {
         name: 'search',
         icon:  search,
+        route: '/search',
     },
     {
-        name: 'heart',
-        icon: 'person',
+        name: 'favorites',
+        icon: heart,
+        route: '/favorites',
     },
     {
         name: 'home',
-        icon: 'home',
+        icon: home,
+        route: '/dashboard',
+    },
+    {
+        name: 'cart',
+        icon: cart,
+        route: '/cart',
+    },
+    {
+        name: 'profile',
+        icon: profile,
+        route: '/profile',
     },
 ];
 
 </script>
 
 <template>
-    <div class="icons">
-        <ul>
-            <li v-for="icon in icons" :key="icon.name">
-                <img :src="icon.icon" :alt="icon.name" />
-            </li>
-        </ul>
-    </div>
+        <div class="navIcons">
+            <RouterLink v-for="button in buttons" :key="button.name" :to="button.route">
+                <component :is="button.icon" class="navIcon" />
+            </RouterLink>
+        </div>
 </template>
 
-<style scoped>
+<style>
 @import "@/assets/sass/navigationBar/_navIcons.scss";
 </style>
