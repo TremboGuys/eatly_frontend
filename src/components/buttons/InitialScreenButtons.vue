de0ca183a67ca52998622eed980d1004a90f3709
LuizBlume <luiz.fernando.251107@gmail.com>
2025-05-24 22:13:17 -0300

    REFACTOR: Adjust in Buttons types #18.

--- src/components/buttons/InitialScreenButtons.vue
+++ src/components/buttons/InitialScreenButtons.vue
@@ -3,8 +3,8 @@ import { RouterLink } from 'vue-router';
 </script>
 <template>
     <div class="container">
-        <router-link class="router" to="/signup"><button type="submit" class="send-button signUp">Cadastrar</button></router-link>
-        <router-link class="router" to="/signin"><button type="submit" class="send-button signIn">Entrar</button></router-link>
+        <router-link class="router" to="/signup"><button class="send-button signUp">Cadastrar</button></router-link>
+        <router-link class="router" to="/signin"><button class="send-button signIn">Entrar</button></router-link>
     </div>
 </template>
 <style scoped>
--- src/views/InitialScreenView.vue
+++ src/views/InitialScreenView.vue
@@ -1,23 +1,24 @@
 <script setup>
 import InitialScreen from '@/components/initialScreen/InitialScreen.vue';
 </script>
-
 <template>
   <main>
     <InitialScreen />
   </main>
 </template>
 <style scoped>
+@media (max-width: 425px) {
+  main {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    background-color: #444801;
+  }
+}
 * {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
-}
-main {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  background-color: #444801;
 }
 </style>
