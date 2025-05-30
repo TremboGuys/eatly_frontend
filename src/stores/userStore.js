ac862d3fb8c6a9d7d17471745c5bbc67e93d5d29
Joaovictor23t4 <joaovictor239090@gmail.com>
2025-05-30 14:32:21 -0300

    REFACTOR: Change user reactive from useUserStore for SignIn/SignUpView

--- src/stores/userStore.js
+++ src/stores/userStore.js
@@ -2,16 +2,6 @@ import { reactive } from 'vue';
 import { defineStore } from 'pinia';
 
 export const useUserStore = defineStore('user', () => {
-    const user = reactive({
-        name: '',
-        genre: '',
-        dateBirth: '',
-        email: '',
-        password: '',
-        cellphone: ''
-    })
 
-    return {
-        user
-    }
+    return;
 })
