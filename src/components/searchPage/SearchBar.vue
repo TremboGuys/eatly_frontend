f61543fccff6c75b1d5780c9932cd51dbb258e75
dnzlucas <lucasdefenix@gmail.com>
2025-06-05 10:41:26 -0300

    FEAT: Create search page (search bar is not yet functional) #27

--- src/components/searchPage/SearchBar.vue
+++ src/components/searchPage/SearchBar.vue
@@ -1,8 +1,19 @@
 <script setup>
+import { ref } from 'vue';
+const search = ref('');
+
+
+function clearSearch() {
+search.value = '';
+}
 </script>
 
 <template>
-  <div></div>
+  <div :class="['input-container', { active: search }]">
+    <input v-model="search" type="text" placeholder="O que você vai pedir hoje?" class="search-input" />
+    <img class="search-icon" src="../../assets/img/navigationBar/search.svg" alt="">
+    <button v-if="search" class="cancel" @click="clearSearch">x</button>
+  </div>
 </template>
 
 <style scoped>
---
+++ src/components/searchPage/SearchBarContainer.vue
@@ -0,0 +1,13 @@
+<script setup>
+import SearchBar from './SearchBar.vue';
+</script>
+
+<template>
+  <div class="search-bar-container">
+    <SearchBar />
+  </div>
+</template>
+
+<style scoped>
+@import "@/assets/sass/searchPage/_SearchBarContainer.scss";
+</style>
