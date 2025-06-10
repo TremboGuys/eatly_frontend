d640a1002f88ebe605bc1a0b88f18e080fce2514
LuizBlume <luiz.fernando.251107@gmail.com>
2025-06-10 20:13:37 -0300

    FEAT: Add images to orderButton #25.

---
+++ src/components/buttons/OrderButton.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { reactive } from 'vue';
+const state = reactive({
+    isOpen: false,
+});
+const dropdown = () => {
+    state.isOpen = !state.isOpen;
+};
+const data = reactive({
+    options: [
+        {
+            id: 1,
+            name: 'Ordenação Padrão',
+            image: '@/assets/img/logo.png',
+            default: true,
+        },
+        {
+            id: 2,
+            name: 'Teste2',
+            image: '@/assets/img/logo.png',
+        },
+    ],
+})
+</script>
+<template>
+    <div class="container">
+        <div class="order-button" @click="dropdown">
+            <button class="btn">Order</button>
+            <div class="orderOptions" v-if="state.isOpen">
+                <div class="option">
+                    <div class="circle">
+                        <img src="@/assets/img/logo.png" alt="">
+                    </div>
+                    <div class="text">
+                        <p>Teste</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<style scoped></style>
