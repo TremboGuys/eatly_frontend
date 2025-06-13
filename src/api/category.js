8d333897b95ef1df710af447639da21dfc9a265e
LuizBlume <luiz.fernando.251107@gmail.com>
2025-06-13 16:37:24 -0300

    FIX: Create CategoryService #27

--- src/api/category.js
+++
@@ -1,8 +0,0 @@
-import { api } from "@/plugins/axios"
-
-export default {
-    async getCategories() {
-      const response = await api.get('/api/categories/');
-      return response.data;
-    }
-  };
