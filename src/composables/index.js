a43f1138c3d6394269bbd98277adacc327c3627d
LuizBlume <luiz.fernando.251107@gmail.com>
2025-08-12 21:16:44 -0300

    FEAT: Finished register owner page #43.

--- src/composables/index.js
+++ src/composables/index.js
@@ -2,6 +2,7 @@ import { useCategoryComposable } from "./category";
 import { useOwnerComposable } from "./owner";
 import { useRestaurantComposable } from "./restaurant"; 
 import { useClientComposable } from "./auth/client";
+import { useRestaurantComposable } from "./restaurant";
 
 useCategoryComposable();
 export {
--- src/services/index.js
+++ src/services/index.js
@@ -3,11 +3,13 @@ import OwnerService from "@/services/owner";
 import RestaurantService from "@/services/restaurant";
 import ClientService from "@/services/auth/client"
 import AuthService from "@/services/auth/auth"
+import RestaurantService from "@/services/restaurant";
 
 export {
     CategoryService,
     OwnerService,
     RestaurantService,
     ClientService,
-    AuthService
+    AuthService,
+    RestaurantService
 };
