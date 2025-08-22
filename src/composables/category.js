561c04687ded6f2da2692b405e60ecf74f0f8e62
Joaovictor23t4 <joaovictor239090@gmail.com>
2025-08-22 16:24:56 -0300

    FIX: Fix category.js in composables folder

--- src/composables/category.js
+++ src/composables/category.js
@@ -1,32 +1,46 @@
 import { ref } from "vue";
 import { CategoryService } from "@/services";
 import { useToastStore } from "@/stores/toastStore";
+
 export const useCategoryComposable = () => {
-    const toastStore = useToastStore();
-    async function getCategories() {
+  const toastStore = useToastStore();
+  async function getCategories() {
 
-        try {
-            const data = await CategoryService.getCategories(); // <- AQUI o await
+    try {
+      const data = await CategoryService.getCategories();
 
-            if (data.length == 0) {
-                toastStore.notify("Nenhuma categoria encontrada.", "error");
-                return [];
-            }
+      if (data.length == 0) {
+        toastStore.notify("Nenhuma categoria encontrada.", "error");
+        return [];
+      }
 
-            console.log(data);
+      return data;
 
-            return data;
-
-        } catch (error) {
-            toastStore.notify(
-                "Erro ao buscar categorias. Por favor, tente novamente.",
-                "error"
-            );
-            console.error("Erro em getCategories:", error);
-            return false;
-        }
-        return {
-            getCategories
-        }
+    } catch (error) {
+      toastStore.notify(
+        "Erro ao buscar categorias. Por favor, tente novamente.",
+        "error"
+      );
+      return false;
     }
-}
+  }
+
+  async function createCategory(category) {
+    try {
+      const categoryFormData = new FormData();
+      categoryFormData.append("name", category.name);
+      categoryFormData.append("file", category.file);
+
+      await CategoryService.createCategory(categoryFormData);
+      toastStore.notify("Categoria criada com sucesso!", "success");
+    } catch(error) {
+      toastStore.notify("Erro ao criar categoria.", "error");
+      return false;
+    }
+  }
+
+  return {
+    getCategories,
+    createCategory
+  };
+};
