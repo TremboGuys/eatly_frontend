d81e79a635b9612f4cfeac55b284bb1be7d6ea14
LuizBlume <luiz.fernando.251107@gmail.com>
2025-08-22 16:46:44 -0300

    FIX: Adjust in code

--- src/components/searchPage/SearchList.vue
+++ src/components/searchPage/SearchList.vue
@@ -2,11 +2,11 @@
 import { ref, onMounted } from 'vue';
 import FoodCategory from './FoodCategory.vue';
 import { useCategory } from '@/composables/category';
-
 const categories = ref([]);
+const { getCategories } = useCategory();
 onMounted(async () => {
-  categories.value = await useCategory().getCategories();
-})
+  categories.value = await getCategories();
+});
 </script>
 <template>
   <div class="list-container">
