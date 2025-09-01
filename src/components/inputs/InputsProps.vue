ad79a232a9e1bff55e90928685078d284435588d
Joaovictor23t4 <joaovictor239090@gmail.com>
2025-09-01 20:31:55 -0300

    FEAT: Connect login in frontend #56

--- src/components/inputs/InputsProps.vue
+++ src/components/inputs/InputsProps.vue
@@ -1,28 +1,38 @@
 <script setup>
-import { useUserStore } from '@/stores';
-
+import { ref, watch } from 'vue';
+import { useClientComposable } from '@/composables';
 const props = defineProps({
     type: String,
     field: String,
     forId: String,
     label: String,
     maxlength: String
-})
+});
 
-const userStore = useUserStore();
+const useClient = useClientComposable();
+
+const textError = ref(false);
 
 const emit = defineEmits(['changeDataUser']);
+
+watch(() => useClient.formState[props.type], () => {
+    textError = useClient.formState[props.type];
+});
 </script>
 <template>
     <div class="container">
         <div class="input">
             <div class="form-group">
-                <input :type="type" class="form-control" :maxlength="maxlength" @input="emit('changeDataUser', {field: props.field, value: $event.target.value})" :placeholder="' '" />
-                <label class="labelInput" :for="forId">{{ label }}</label>
+                <input :type="type" class="form-control" :maxlength="maxlength" @input="emit('changeDataUser', {field: props.field, value: $event.target.value})" :placeholder="' '" ref="input" :class="textError ? 'input-error' : ''" />
+                <label class="labelInput" :for="forId" :style="(textError) ? 'color: red' : ''">{{ label }}</label>
             </div>
         </div>
     </div>
 </template>
 <style scoped>
 @import '@/assets/sass/inputs/_inputsProps.scss';
+
+.input-error {
+ border: 1px solid red;
+}
 </style>
---
+++ src/metaDatas/errors/index.js
@@ -0,0 +1,5 @@
+export const registerClient = [{
+    email: "Seu e-mail não está no padrão correto!",
+    password: "Escreva uma senha com no mínimo 8 dígitos, 1 Maiúscula, 1 minúscula e 1 caracter especial (!*&%$#",
+    date_birth: "Usuários menores de 16 anos são proíbidos!"
+}]
