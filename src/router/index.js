da90680d7f3fc7da5a25c4944503ac3288a28bdb
LuizBlume <luiz.fernando.251107@gmail.com>
2025-09-02 17:52:10 -0300

    FEAT: Create banner restaurant page #61.

--- src/router/index.js
+++ src/router/index.js
@@ -39,7 +39,7 @@ const router = createRouter({
           component: () => import("@/views/CartView.vue")
         },
         {
-          path: '/restaurant/:id',
+          path: '/restaurant/',
           name: 'restaurant',
           component: () => import('@/views/restaurant/RestaurantView.vue')
         },
