33a84c2312337e653d8ffbf1afc7c9dcb80c501d
LuizBlume <luiz.fernando.251107@gmail.com>
2025-09-02 16:07:06 -0300

    FEAT: Create banner restaurant page #61.

---
+++ src/components/restaurantPage/banner/BannerComp.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+
+const bannerImage = ref('');
+
+onMounted(async () => {
+    const response = await axios.get('your-api-endpoint');
+    bannerImage.value = response.data.imageUrl;
+});
+</script>
+<template>
+    <div class="container">
+        <img class="banner" :src="bannerImage" src="@/assets/banner.png" alt="banner"/>
+    </div>
+</template>
+<style scoped>
+</style>
