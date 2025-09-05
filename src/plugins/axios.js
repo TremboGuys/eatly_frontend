72b09af68c4adfb9bfc6ce103ca74bcd26989fe7
Joaovictor23t4 <joaovictor239090@gmail.com>
2025-09-05 14:39:31 -0300

    FIX: Add /api/ on baseURL #56

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  // baseURL: 'https://eatly-backend-cbai.onrender.com/',
+  baseURL: 'https://eatly-backend-cbai.onrender.com/api/',
   baseURL: 'http://127.0.0.1:8000/api/',
   withCredentials: true,
   headers: {
