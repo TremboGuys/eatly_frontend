b6281c68015da2c3151b561ef2267fb98d0a86e6
LuizBlume <luiz.fernando.251107@gmail.com>
2025-09-05 14:00:02 -0300

    FIX: Fix in URL.

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  // baseURL: 'https://eatly-backend-cbai.onrender.com/',
-  baseURL: 'http://127.0.0.1:8000/api/',
+  baseURL: 'https://eatly-backend-cbai.onrender.com/',
+  // baseURL: 'http://127.0.0.1:8000/api/',
   withCredentials: true,
   headers: {
     Authorization: `${localStorage.getItem('refresh') != 'undefined' ? 'Bearer ' + localStorage.getItem('refresh') : ''}`
