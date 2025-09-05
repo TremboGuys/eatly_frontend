bb94b805ce6b7f2895802bdae23bdf99815d241c
LuizBlume <luiz.fernando.251107@gmail.com>
2025-09-05 12:51:24 -0300

    FEAT: Create component for option #61.

---
+++ src/components/restaurantPage/options/OptionsScroll.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue';
+
+const options = ref([
+    "MAIS PEDIDOS",
+    "GOURMET",
+    "PASTÉIS",
+    "BEBIDAS",
+    "SOBREMESAS"
+])
+
+const activeTab = ref(1)
+function SetActive(index) {
+    activeTab.value = index
+}
+</script>
+<template>
+    <div class="container">
+        <div class="options" ref="scrollContainer">
+            <div v-for="(option, index) in options" :key="index" :class="[]"></div>
+        </div>
+    </div>
+</template>
