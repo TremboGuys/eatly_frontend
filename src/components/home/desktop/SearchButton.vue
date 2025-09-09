98ffc9d34d7f49fa0baa3d8905fcaf01c470f056
dnzlucas <lucasdefenix@gmail.com>
2025-09-09 16:40:59 -0300

    FEAT: Create Header for desktop size

---
+++ src/components/home/desktop/SearchButton.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+import search from '@/assets/img/navigationBar/search.svg?component';
+
+const button = {
+    icon: search
+};</script>
+
+<template>
+    <RouterLink to="/search">
+    <component :is="button.icon" class="navIcon" />
+</RouterLink>
+</template>
