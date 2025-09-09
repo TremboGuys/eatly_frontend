ead78316b9b55269c5594bf7ca107db7c393f8c3
dnzlucas <lucasdefenix@gmail.com>
2025-09-09 16:43:25 -0300

    FEAT: Add conditional display logic for header based on screen width

--- src/components/home/HomeContainer.vue
+++ src/components/home/HomeContainer.vue
@@ -1,14 +1,17 @@
 <script setup>
+import { useWindowSize } from '@vueuse/core'
 import LogoArea from './LogoArea.vue';
 import BestSellersContainer from './bestsellers/BestSellersContainer.vue';
 import HomeCarousel from './carousel/HomeCaroulselContainer.vue';
 import HomeCategories from './categories/HomeCategories.vue';
+import HeaderDesktop from './desktop/HeaderDesktop.vue';
+
+const {width} = useWindowSize();
 </script>
 
 <template>
-
-    <LogoArea />
-    <HomeCategories />
+    <component :is="width >= 425 ? HeaderDesktop : LogoArea" />
+        <HomeCategories />
     <HomeCarousel />
     <BestSellersContainer />
 </template>
