eba19eadf9c7e4c24ebb2053cf1982fd369a4f08
LuizBlume <luiz.fernando.251107@gmail.com>
2025-09-13 23:15:57 -0300

    FIX: Atualiza rotas para incluir parâmetros dinâmicos de ID #79.

--- src/router/index.js
+++ src/router/index.js
@@ -39,7 +39,7 @@ const router = createRouter({
           component: () => import("@/views/CartView.vue")
         },
         {
-          path: '/restaurant/',
+          path: '/restaurant/:id',
           name: 'restaurant',
           component: () => import('@/views/restaurant/RestaurantView.vue')
         },
@@ -54,7 +54,7 @@ const router = createRouter({
           component: () => import('@/views/restaurant/CreateReviewView.vue')
         },
         {
-          path: '/product/',
+          path: '/product/:id',
           name: 'product',
           component: () => import('@/views/restaurant/ProductView.vue')
         }
