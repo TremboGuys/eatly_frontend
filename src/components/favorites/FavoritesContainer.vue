bfa8097ca269b6749b77fd1660c5ca55cd7d4998
dnzlucas <lucasdefenix@gmail.com>
2025-10-12 18:03:36 -0300

    FEAT: Create Favorites page files #102

---
+++ src/components/favorites/FavoritesContainer.vue
@@ -0,0 +1,29 @@
+<script setup>
+import BestSellersList from '../home/bestsellers/BestSellersList.vue';
+import ListItem from '@/components/home/bestsellers/ListItem.vue';
+import { useRestaurantStore } from "@/stores";
+
+const restaurantStore = useRestaurantStore();
+
+</script>
+<template>
+    <div class="favorites-container">
+        <div class="titleSection">
+            <span class="text">Favoritos</span>
+            <div class="hr"></div>
+        </div>
+        <div class="lista">
+            <ListItem v-for="(restaurant, index) in restaurantStore.restaurants" :key="index" :restaurant="restaurant" @click="restaurantStore.createRestaurantView({restaurant: restaurant.id})" />
+
+        </div>
+    </div>
+</template>
+
+<style>
+@import "@/assets/sass/favorites/_favoritesContainer.scss";
+.lista{
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+}
+</style>
