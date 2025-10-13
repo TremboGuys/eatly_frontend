f8bcd7180de9a855717ca0d48f507fa2676592ea
LuizBlume <luiz.fernando.251107@gmail.com>
2025-10-13 15:19:31 -0300

    FEAT: Create new logic for inputs card #107.

--- src/components/payment/SelectOption.vue
+++ src/components/payment/SelectOption.vue
@@ -4,11 +4,13 @@ defineProps({
 })
 </script>
 <template>
-    <div class="container">
-        <button @click="$emit('selectMethod', 'credito')" :class="{ active: selected === 'credito' }">Cartão de Crédito</button>
-        <button @click="$emit('selectMethod', 'debito')" :class="{ active: selected === 'debito' }">Cartão de Débito</button>
-        <button @click="$emit('selectMethod', 'pix')" :class="{ active: selected === 'pix' }">Pix</button>
-    </div>
+  <div class="container">
+    <button @click="$emit('selectMethod', 'credito')" :class="{ active: selected === 'credito' }">Cartão de
+      Crédito</button>
+    <button @click="$emit('selectMethod', 'debito')" :class="{ active: selected === 'debito' }">Cartão de
+      Débito</button>
+    <button @click="$emit('selectMethod', 'pix')" :class="{ active: selected === 'pix' }">Pix</button>
+  </div>
 </template>
 <style scoped>
 @import "@/assets/sass/payment/_selectOption.scss";
