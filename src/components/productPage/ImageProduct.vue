4f9e686b56b6bf71d4ad4d3477933938a3a75d96
LuizBlume <luiz.fernando.251107@gmail.com>
2025-10-17 16:53:27 -0300

    REFACTOR: Initial refactor #117.

--- src/components/productPage/ImageProduct.vue
+++ src/components/productPage/ImageProduct.vue
@@ -16,37 +16,6 @@ const currentIndex = ref(0);
 const isDragging = ref(false);
 const startX = ref(0);
 const translate = ref(0);
-
-const onPointerDown = (e) => {
-    isDragging.value = true;
-    startX.value = e.clientX;
-    e.currentTarget.setPointerCapture?.(e.pointerId);
-};
-const onPointerMove = (e) => {
-    if (!isDragging.value) return;
-    translate.value = e.clientX - startX.value;
-};
-const onPointerUp = (e) => {
-    if (!isDragging.value) return;
-    isDragging.value = false;
-
-    const diff = translate.value;
-    if (diff < -50) nextImage();
-    else if (diff > 50) prevImage();
-
-    translate.value = 0;
-    e.currentTarget.releasePointerCapture?.(e.pointerId);
-};
-const onPointerCancel = (e) => {
-    isDragging.value = false;
-    translate.value = 0;
-    if (e?.pointerId) e.currentTarget.releasePointerCapture?.(e.pointerId);
-};
-const nextImage = () =>
-    (currentIndex.value = (currentIndex.value + 1) % images.length);
-const prevImage = () =>
-(currentIndex.value =
-    (currentIndex.value - 1 + images.length) % images.length);
 const trackStyle = computed(() => {
     const percent = -currentIndex.value * 100;
     return {
@@ -61,12 +30,8 @@ const trackStyle = computed(() => {
             <router-link :to="`/restaurant/${props.restaurant.id}`" class="back-link">
                 <i class="fa-solid fa-arrow-left"></i>
             </router-link>
-            <div class="nameRestaurant">
-                <span class="name">{{ props.restaurant.name }}</span>
-            </div>
         </div>
-        <div class="carousel" @pointerdown="onPointerDown" @pointermove="onPointerMove" @pointerup="onPointerUp"
-            @pointercancel="onPointerCancel" @mouseleave="onPointerCancel">
+        <div class="carousel">
             <div class="track" :style="trackStyle">
                 <!-- <div class="slide" v-for="(img, i) in product.url_field" :key="i">
                     <img :src="img" class="image" alt="produto" draggable="false" />
--- src/components/productPage/TitleProduct.vue
+++ src/components/productPage/TitleProduct.vue
@@ -6,24 +6,11 @@ const props = defineProps({
   name: { type: String, required: true },
   idProduct: { type: String, required: true }
 });
-const toggleActive = async () => {
-  favoriteStore.isFavorite = !favoriteStore.isFavorite;
-
-  const response = await favoriteStore.createFavorite({ product: props.idProduct });
-
-  if (!response) {
-    favoriteStore.isFavorite = !favoriteStore.isFavorite;
-  }
-};
 </script>
 <template>
   <div class="container">
     <h1>{{ props.name }}</h1>
-    <i 
-      class="fa-heart" 
-      :class="favoriteStore.isFavorite ? 'fa-solid active' : 'fa-regular'" 
-      @click="toggleActive"
-    ></i>
+    <div class="hr"></div>
   </div>
 </template>
 
