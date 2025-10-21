08c4c7468211a6bc82482e277bf14e4df744e9c7
lunabolsoni <bolsoniluna@gmail.com>
2025-10-21 09:29:14 -0300

    feat 106: main

--- src/components/coupon/CouponPageContainer.vue
+++ src/components/coupon/CouponPageContainer.vue
@@ -21,20 +21,34 @@ const tab = ref('cupons');
         <span class="status" :class="{'is-active': tab == 'cupons'}" @click="tab='cupons'">Seus cupons</span>
       </div>
     </div>
-      <div class="line" v-if="tab == 'available'">
-        <p>blab</p>
-        <p>Disponíveis.</p>
+      
+  </header>
+  <main class="couponMainContainer"> 
+
+<div class="line" v-if="tab == 'available'">
+      <div class="card">
+  <div class="icon"></div>
+  <div class="info">
+    <h3>50% de desconto na sua primeira compra!!!!</h3>
+    <p>em qualquer produto</p>
+    <div class="validade">
+      <span class="date">Valido até:
+     30/12/2024</span>
+    </div>
+  </div>
+  <button class="resgate">resgate agora</button>
+</div>
       </div>
       <div class="line" v-else>
-        <p>blab</p>
-        <p>Meus cupons.</p>
+        <p>nenhum cupom resgatado</p>
       </div>
-  </header>
+
+  </main>
 </template>
 
 
 <style scoped>
-@import '@/assets/sass/coupon/_couponPageContainer.scss';
+@import '@/assets/sass/coupon/_couponMainContainer.scss';
 @import '@/assets/sass/coupon/_couponHeaderContainer.scss';
 
 .status {
@@ -48,5 +62,12 @@ span.is-active {
   border-bottom: 2px solid #4c4b00;
   color: #4c4b00 !important;
 }
-
+.line {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-family: 'poppins', sans-serif;
+  font-weight: bold;
+  font-size: 10px;
+}
 </style>
