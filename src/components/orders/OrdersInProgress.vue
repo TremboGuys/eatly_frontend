3c0aec5be4973733152c95864c1902e43879be81
LuizBlume <luiz.fernando.251107@gmail.com>
2025-10-24 16:50:36 -0300

    REFACTOR: Refactor orders page #120.

--- src/components/orders/OrdersInProgress.vue
+++ src/components/orders/OrdersInProgress.vue
@@ -1,42 +1,49 @@
 <script setup>
 import { ref } from 'vue';
+
 const cart = ref([
-    {
-        id: 1,
-        title: "Aborgue assado",
-        url_file: "https://via.placeholder.com/150",
-        price: 19.90,
-        quantity: 2
-    },
-    {
-        id: 2,
-        title: "Lanche gostoso 123",
-        url_file: "https://via.placeholder.com/150",
-        price: 9.90,
-        quantity: 1
-    }
+  {
+    id: 1,
+    restaurant: "Hamburgueria Ponto X",
+    status: "Pedido em andamento",
+    items: "1 X-Burguer, 1 Coca-Cola",
+    logo: new URL('@/assets/img/banner.png', import.meta.url).href,
+    image: new URL('@/assets/img/pizza.png', import.meta.url).href,
+  }
 ]);
 </script>
 <template>
-    <div class="container">
-        <main class="orderMainContainer">
-            <div v-if="cart.length === 0" class="card empty">
-                Nenhum pedido realizado
+  <div class="container">
+    <main class="orderMainContainer">
+      <div v-if="cart.length === 0" class="card empty">
+        Sem pedidos recentes por aqui
+      </div>
+      <div v-else>
+        <h2 class="sectionTitle">Histórico</h2>
+        <div v-for="item in cart" :key="item.id" class="card">
+          <div class="orderHeader">
+            <div class="restaurantInfo">
+              <img class="logo" :src="item.logo" alt="Logo" />
+              <div class="details">
+                <h3>{{ item.restaurant }}</h3>
+                <p class="status">
+                  {{ item.status }}
+                </p>
+              </div>
             </div>
-            <div v-else>
-                <div v-for="item in cart" :key="item.id" class="card">
-                    <div class="icon">
-                        <img :src="item.url_file" :alt="item.title" />
-                    </div>
-                    <div class="info">
-                        <h3>{{ item.title }}</h3>
-                        <p>Quantidade: {{ item.quantity }}</p>
-                        <span class="price">R$ {{ (item.price * item.quantity).toFixed(2).replace('.', ',') }}</span>
-                    </div>
-                </div>
-            </div>
-        </main>
-    </div>
+            <p class="date">{{ item.date }}</p>
+          </div>
+          <div class="orderBody">
+            <div class="itemText">{{ item.items }}</div>
+            <img :src="item.image" alt="item" class="itemImg" />
+          </div>
+          <div class="orderActions">
+            <button class="add">Adicione ao carrinho</button>
+          </div>
+        </div>
+      </div>
+    </main>
+  </div>
 </template>
 <style scoped>
 @import '@/assets/sass/orders/_ordersCards.scss';
