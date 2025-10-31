b6c4d5b38c2a7a90062264216ed4522490ca4fda
dnzLucas <lucasdefenix@gmail.com>
2025-10-30 22:54:50 -0300

    68 / 5.000 feat: Update reviews and improve styles on the restaurant page

--- src/components/restaurantPage/menu/ItemsMenu.vue
+++ src/components/restaurantPage/menu/ItemsMenu.vue
@@ -1,22 +1,22 @@
 <script setup>
-import { ref, onMounted, onBeforeUnmount } from 'vue';
-import { menu } from '@/metaDatas/menuData';
-import { useRestaurantStore } from '@/stores/';
-import { useRoute, useRouter } from 'vue-router';
+import { ref, onMounted, onBeforeUnmount } from "vue";
+import { menu } from "@/metaDatas/menuData";
+import { useRestaurantStore } from "@/stores/";
+import { useRoute, useRouter } from "vue-router";
 
 const menuItems = ref(menu);
 const restaurantStore = useRestaurantStore();
 
 const categoryRefs = ref([]);
 const activeIndex = ref(0);
-const emit = defineEmits(['update:activeIndex']);
+const emit = defineEmits(["update:activeIndex"]);
 
 function scrollToCategory(index) {
   const el = categoryRefs.value[index];
   if (el) {
-    el.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    el.scrollIntoView({ behavior: "smooth", block: "start" });
     activeIndex.value = index;
-    emit('update:activeIndex', index);
+    emit("update:activeIndex", index);
   }
 }
 
@@ -27,15 +27,15 @@ function onScroll() {
     if (rect.top <= 150 && rect.bottom > 150) {
       if (activeIndex.value !== i) {
         activeIndex.value = i;
-        emit('update:activeIndex', i);
+        emit("update:activeIndex", i);
       }
       break;
     }
   }
 }
 
-onMounted(() => window.addEventListener('scroll', onScroll));
-onBeforeUnmount(() => window.removeEventListener('scroll', onScroll));
+onMounted(() => window.addEventListener("scroll", onScroll));
+onBeforeUnmount(() => window.removeEventListener("scroll", onScroll));
 
 defineExpose({ scrollToCategory });
 
@@ -43,17 +43,43 @@ const router = useRouter();
 const route = useRoute();
 </script>
 <template>
-    <div class="container">
-        <div v-for="(category, categoryIndex) in restaurantStore.restaurant.products" :key="categoryIndex" class="categorySection" :ref="el => categoryRefs[categoryIndex] = el">
-            <div class="titleSection">
-                <span class="text">{{ category.category }}</span>
-                <div class="hr"></div>
-            </div>
+  <div class="container">
+    <div
+      v-for="(category, categoryIndex) in restaurantStore.restaurant.products"
+      :key="categoryIndex"
+      class="categorySection"
+      :ref="(el) => (categoryRefs[categoryIndex] = el)"
+    >
+      <div class="titleSection">
+        <span class="text">{{ category.category }}</span>
+        <div class="hr"></div>
+      </div>
 
-            <div class="container-product" v-for="(product, productIndex) in category.products" :key="productIndex">
-              <router-link class="itemLink" :to="`/restaurant/${route.params.id}/product/${product.id}`">
-                <div class="item">
-                    <div class="top">
+      <div
+        class="container-product"
+        v-for="(product, productIndex) in category.products"
+        :key="productIndex"
+      >
+        <router-link
+          class="itemLink"
+          :to="`/restaurant/${route.params.id}/product/${product.id}`"
+        >
+          <div class="item">
+            <div class="top">
+              <div class="image">
+                <img :src="product.url_file" :alt="product.name" />
+              </div>
+              <div class="info">
+                <div class="title">{{ product.name }}</div>
+                <div class="description">{{ product.description }}</div>
+                <div class="bottom">
+              <div class="price">
+                R${{ product.price.toFixed(2).toString().replace(".", ",") }}
+              </div>
+              </div>
+            </div>
+            </div>
+            <!-- <div class="top">
                         <div class="image">
                             <img :src="product.url_file" :alt="product.name">
                         </div>
@@ -61,15 +87,17 @@ const route = useRoute();
                             <div class="title">{{ product.name }}</div>
                             <div class="description">{{ product.description }}</div>
                         </div>
-                    </div>
-                    <div class="bottom">
-                        <div class="price">R${{ product.price.toFixed(2).toString().replace(".", ",") }}</div>
-                    </div>
-                  </div>
-                </router-link>
-            </div>
-        </div>
+                    </div> -->
+            <!-- <div class="bottom">
+              <div class="price">
+                R${{ product.price.toFixed(2).toString().replace(".", ",") }}
+              </div>
+            </div> -->
+          </div>
+        </router-link>
+      </div>
     </div>
+  </div>
 </template>
 <style scoped>
 @import "@/assets/sass/restaurantPage/menu/_itemsMenu.scss";
