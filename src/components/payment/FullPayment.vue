1ae5818b030909b06522b199ce008aa087c32ad0
Joaovictor23t4 <joaovictor239090@gmail.com>
2025-10-30 21:56:25 -0300

    FEAT: Add PuffSpinner #163

--- src/components/payment/FullPayment.vue
+++ src/components/payment/FullPayment.vue
@@ -1,5 +1,10 @@
 <script setup>
 import { computed } from "vue";
+import { useCartStore, usePaymentStore } from "@/stores";
+
+const cartStore = useCartStore();
+const paymentStore = usePaymentStore();
+
 const props = defineProps({
     orderTotal: { type: Number, required: true },
     deliveryFee: { type: Number, required: true },
@@ -14,20 +19,20 @@ const total = computed(
         <div class="summary-box">
             <div class="row">
                 <span>Total do Pedido:</span>
-                <strong>R$ {{ orderTotal.toFixed(2) }}</strong>
+                <strong>R$ {{ cartStore.cart[paymentStore.state.indexOrder].totalValue }}</strong>
             </div>
-            <div class="row">
+            <!-- <div class="row">
                 <span>Entrega:</span>
                 <strong>R$ {{ deliveryFee.toFixed(2) }}</strong>
-            </div>
+            </div> -->
             <div class="row">
                 <span>Desconto:</span>
-                <strong class="discount">- R$ {{ couponDiscount.toFixed(2) }}</strong>
+                <strong class="discount">- R$ 0,00</strong>
             </div>
             <div class="divider"></div>
             <div class="row total">
                 <span class="total">Total a pagar:</span>
-                <strong>R$ {{ total.toFixed(2) }}</strong>
+                <strong>R$ {{ cartStore.cart[paymentStore.state.indexOrder].totalValue }}</strong>
             </div>
         </div>
     </div>
--- src/components/payment/PaymentScreen.vue
+++ src/components/payment/PaymentScreen.vue
@@ -2,6 +2,8 @@
 import { ref, computed } from 'vue';
 import { useCartStore, usePaymentStore } from '@/stores';
 import FullPayment from './FullPayment.vue';
+import { VueSpinnerPuff } from 'vue3-spinners';
+import { useClipboard } from '@vueuse/core';
 
 const selectedMethod = ref(null)
 const cartStore = useCartStore();
@@ -51,16 +53,16 @@ function submitCard() {
     </div>
     <div v-else-if="props.method === 'pix'" class="pix-section">
       <h2>Pagamento via Pix</h2>
-      <button @click="paymentStore.createPayment" class="btn" :disabled="paymentStore.state.loading">Pagar com
-        Pix</button>
-      <div v-if="paymentStore.payment?.qr_code_base64" class="qr-code">
+      <button @click="paymentStore.createPayment" class="btn" :disabled="paymentStore.state.loading">Pagar com Pix</button>
+      <div v-if="!paymentStore.state.createPayment && paymentStore.payment?.qr_code_base64" class="qr-code">
         <h3>Escaneie com seu app Pix:</h3>
         <img :src="'data:image/png;base64,' + paymentStore.payment.qr_code_base64" alt="QR Code Pix" />
-        <p><strong>Copia e cola:</strong> <span class="pix-code">{{ paymentStore.payment.qr_code }}</span></p>
+        <p><strong>Copia e cola:</strong> <span class="pix-code" @click="paymentStore.copyQrCodeClipboard">{{ paymentStore.payment.qr_code }}</span></p>
       </div>
     </div>
+    <VueSpinnerPuff v-if="paymentStore.state.createPayment"  color="#444801" size="120" />
   </div>
-    <FullPayment v-if="selectedMethod" :orderTotal="150.00" :deliveryFee="15.00" :couponDiscount="20.00" />
+  <FullPayment v-if="selectedMethod" :orderTotal="150.00" :deliveryFee="15.00" :couponDiscount="20.00" />
 </template>
 
 <style scoped>
